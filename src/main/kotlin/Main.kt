fddeade8ed9935cb7ae5ea250ff99f7321d2c960
sorbris <sorbris@users.noreply.github.com>
2022-12-02 08:55:37 +0100

    Day 2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,9 @@
 import day01.Day01
+import day02.Day02
 
 fun main(args: Array<String>) {
 
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    Day01.problem1()
-    Day01.problem2()
+    Day02.problem1()
+    Day02.problem2()
 }
---
+++ src/main/kotlin/day02/Day02.kt
@@ -0,0 +1,40 @@
+package day02
+
+object Day02 {
+    fun problem1() {
+        val results = javaClass.getResource("/day02.txt")!!.readText().lines().map {
+            val moves = it.split(" ")
+            score(ops1[moves[0]]!!, ops1[moves[1]]!!)
+        }
+        val score = results.fold(0) { acc, points ->
+            acc + points
+        }
+        println("score is: $score")
+    }
+
+    private val ops1 = mapOf("A" to 0, "B" to 1, "C" to 2, "X" to 0, "Y" to 1, "Z" to 2)
+
+    private fun score(opMove: Int, myMove: Int): Int {
+        return if (opMove == myMove) {
+            myMove + 1 + 3
+        } else if ((opMove + 1).mod(3) == myMove) {
+            myMove + 1 + 6
+        } else {
+            myMove + 1
+        }
+    }
+
+    fun problem2() {
+        val results = javaClass.getResource("/day02.txt")!!.readText().lines().map {
+            val moves = it.split(" ")
+            val opMove = ops2[moves[0]]!!
+            val myMove = (opMove + ops2[moves[1]]!!).mod(3)
+            score(opMove, myMove)
+        }
+        val score = results.fold(0) { acc, points ->
+            acc + points
+        }
+        println("score is: $score")
+    }
+    private val ops2 = mapOf("A" to 0, "B" to 1, "C" to 2, "X" to 2, "Y" to 0, "Z" to 1)
+}
