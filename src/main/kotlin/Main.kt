a56ea5dbae92ccbc45030ce86a66633b7edfafb0
sorbris <sorbris@users.noreply.github.com>
2022-12-03 09:03:59 +0100

    Day 3

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,10 @@
 import day01.Day01
 import day02.Day02
+import day03.Day03
 
 fun main(args: Array<String>) {
 
 
-    Day02.problem1()
-    Day02.problem2()
+    Day03.problem1()
+    Day03.problem2()
 }
---
+++ src/main/kotlin/day03/Day03.kt
@@ -0,0 +1,42 @@
+package day03
+
+object Day03 {
+    fun problem1() {
+        var sums = 0
+        javaClass.getResource("/day03.txt")!!.readText().lines().asSequence().map { it.toCharArray() }
+            .forEach { array ->
+                val half = array.size/2
+                val matches = mutableSetOf<Char>()
+                for (i in array.size/2..array.lastIndex) {
+                    val char = array[i]
+                    for (j in 0 until half) {
+                        if (array[j] == char) {
+                            matches.add(char)
+                            break
+                        }
+                    }
+                }
+
+                for (char in matches) {
+                    sums += if (char.isLowerCase()) char - '`' else char - '@' + 26
+                }
+            }
+        println("sum $sums")
+    }
+
+    fun problem2() {
+        val badges = mutableListOf<Char>()
+        javaClass.getResource("/day03.txt")!!.readText().lines().asSequence().windowed(3, 3, true).forEach {  members ->
+            for (c in members[0]) {
+                if (members[1].contains(c, ignoreCase = false) && members[2].contains(c, ignoreCase = false)) {
+                    badges.add(c)
+                    break
+                }
+            }
+        }
+        val sum = badges.fold(0) {acc, char ->
+            acc + if (char.isLowerCase()) { char - '`' } else { char - '@' + 26 }
+        }
+        println("Sum: $sum")
+    }
+}
