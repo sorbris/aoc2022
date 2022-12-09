595e38260e0997a1284c1083e58168f4602d4dfd
sorbris <sorbris@users.noreply.github.com>
2022-12-09 19:45:47 +0100

    Day 09 cleanup

--- src/main/kotlin/day09/Day09.kt
+++ src/main/kotlin/day09/Day09.kt
@@ -4,51 +4,24 @@ import Day
 import kotlin.math.abs
 
 object Day09 : Day {
+
     override fun problem1() {
-        val places = mutableSetOf<Pair<Int, Int>>()
-        javaClass.getResource("/day09.txt")!!.readText().lineSequence()
-            .map { it.split(" ") }
-            .map { it.first() to it.last().toInt() }
-            .fold((0 to 0) to (0 to 0)) { acc, action ->
-                val (head, tail) = acc
-                var (hx, hy) = head
-                var (tx, ty) = tail
-                val (dir, steps) = action
-                for (i in 1..steps) {
-                    when (dir) {
-                        "U" -> hx += 1
-                        "D" -> hx -= 1
-                        "L" -> hy -= 1
-                        "R" -> hy += 1
-                    }
-                    val adjusted = adjustPosition(hx to hy, tx to ty)
-                    tx = adjusted.first
-                    ty = adjusted.second
-                    places.add(tx to ty)
-                }
-
-                (hx to hy) to (tx to ty)
-            }
-        println("${places.size} visited")
-    }
-
-    private fun adjustPosition(head: Pair<Int, Int>, tail: Pair<Int, Int>): Pair<Int, Int> {
-        val dx = head.first - tail.first
-        val dy = head.second - tail.second
-        return if (abs(dx) > 1 || abs(dy) > 1) {
-            tail.first + Integer.signum(dx) to tail.second + Integer.signum(dy)
-        } else {
-            tail
-        }
+        val places = countPlaces(javaClass.getResource("/day09.txt")!!.readText(), 1)
+        println("$places visited")
     }
 
     override fun problem2() {
-        val places = javaClass.getResource("/day09.txt")!!.readText().lineSequence()
+        val places = countPlaces( javaClass.getResource("/day09.txt")!!.readText(), 9)
+        println("problem2: $places")
+    }
+
+    private fun countPlaces(input: String, tails: Int): Int {
+        return input.lineSequence()
             .map { it.split(" ") }
             .map { it.first() to it.last().toInt() }
             .map { cmd -> (1..cmd.second).map { cmd.first } }
             .flatten()
-            .runningFold((1 .. 10).map { 0 to 0 }) { acc, action ->
+            .runningFold((0 .. tails).map { 0 to 0 }) { acc, action ->
                 var (x, y) = acc.first()
                 when (action) {
                     "U" -> x += 1
@@ -57,12 +30,16 @@ object Day09 : Day {
                     "R" -> y += 1
                 }
                 acc.drop(1).runningFold(x to y) { head, tail ->
-                    adjustPosition(head, tail)
+                    val dx = head.first - tail.first
+                    val dy = head.second - tail.second
+                    if (abs(dx) > 1 || abs(dy) > 1) {
+                        tail.first + Integer.signum(dx) to tail.second + Integer.signum(dy)
+                    } else {
+                        tail
+                    }
                 }
             }.map {
                 it.last()
             }.toSet().size
-
-        println("problem2: $places")
     }
 }
