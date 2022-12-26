d05a5d098551f1732e10f75e9840c44021147997
sorbris <sorbris@users.noreply.github.com>
2022-12-27 00:28:14 +0100

    working problem 1

--- src/main/kotlin/day17/Day17.kt
+++ src/main/kotlin/day17/Day17.kt
@@ -1,72 +1,162 @@
 package day17
 
 import Day
+import java.util.Objects
 import kotlin.math.max
 import kotlin.math.min
 
 object Day17 : Day {
 
     private val rocks = listOf(
-        Rock(intArrayOf(1, 1, 1, 1), intArrayOf(0, 0, 0, 0)),
-        Rock(intArrayOf(2, 3, 2), intArrayOf(-1, 0, -1)),
-        Rock(intArrayOf(1, 1, 3), intArrayOf(0, 0, 0)),
-        Rock(intArrayOf(4), intArrayOf(0)),
-        Rock(intArrayOf(2, 2), intArrayOf(0, 0))
+        Rock(listOf(booleanArrayOf(true), booleanArrayOf(true), booleanArrayOf(true), booleanArrayOf(true))),
+        Rock(listOf(booleanArrayOf(false, true, false), booleanArrayOf(true, true, true), booleanArrayOf(false, true, false))),
+        Rock(listOf(booleanArrayOf(true, false, false), booleanArrayOf(true, false, false), booleanArrayOf(true, true, true))),
+        Rock(listOf(booleanArrayOf(true, true, true, true))),
+        Rock(listOf(booleanArrayOf(true, true), booleanArrayOf(true, true)))
     )
     override fun problem1() {
-        val chamber = IntArray(7) { 0 }
-        var dropY = 4
-
-        val pattern = javaClass.getResource("/day17-test.txt")!!.readText().map {
+        val pattern = javaClass.getResource("/day17.txt")!!.readText().map {
             if (it == '<') -1
             else 1
         }
         val winds = Winds(pattern)
-        var maxY = 0
-        for (i in 0..3) {
-            dropRock(rocks[i.mod(rocks.size)], dropY, chamber, winds)
-            maxY = chamber.fold(Integer.MIN_VALUE) { acc, h ->
-                max(acc, h)
-            }
-            dropY = maxY + 4
+        val chamber = listOf(
+            RockColumn(),
+            RockColumn(),
+            RockColumn(),
+            RockColumn(),
+            RockColumn(),
+            RockColumn(),
+            RockColumn(),
+        )
+        var y = 3
+
+        val fulls = mutableListOf<Int>()
+        for (i in 0..2021) {
+            val index = i.mod(rocks.size)
+            val (ny, fy) = dropRock(rocks, index, y, chamber, winds)
+            y = ny + 3
+            if (fy != -1) fulls.add(fy)
         }
-        println("max: $maxY")
+        println("max: ${y - 3}")
     }
 
-    private fun dropRock(rock: Rock, y: Int, chamber: IntArray, winds: Winds) {
+    private fun dropRock(rocks: List<Rock>, rockIndex: Int, y: Int, chamber: List<RockColumn>, winds: Winds, hashes: MutableList<Int>? = null): Pair<Int, Int> {
+        val rock = rocks[rockIndex]
         var ry = y
         var rx = 2
         var resting = false
+        if (hashes != null) {
+            val hash = Objects.hash(winds.i, rockIndex, chamber.map { it[y - 3] })
+            hashes.add(hash)
+        }
         while (!resting) {
             val direction = winds.getNext()
-            rx += direction
-            rx = max(rx, 0)
-            rx = min(rx, 7 - rock.width())
-            resting = checkHitbox(rock, rx, ry - 1, chamber)
-            --ry
-            if (resting) {
-                for (i in rock.heights.indices) {
-                    chamber[rx + i] = ry + rock.heights[i]
+            // first check if we are inside the bounds
+            if (rx + direction >= 0 && rx + direction <= 6 - rock.cols.lastIndex) {
+                // if still within three steps just move
+                if (y - ry < 3) {
+                    rx += direction
+                } else {
+                    var blocked = false
+                    outer@ for (i in rock.cols.indices) {
+                        for (j in rock.cols[i].indices) {
+                            if (chamber[rx + direction + i][ry + j].and(rock.cols[i][j])) {
+                                blocked = true
+                                break@outer
+                            }
+                        }
+                    }
+                    if (!blocked) rx += direction
+
                 }
             }
-        }
-    }
 
-    private fun checkHitbox(rock: Rock, x: Int, y: Int, chamber: IntArray): Boolean {
-        for (i in rock.hitbox.indices) {
-            if (y + rock.hitbox[i] == chamber[x + i]) {
-                return true
+            // Now check if we will rest
+            if (ry - 1 < 0) {
+                resting = true
+            } else if (y - ry >= 3){
+                outer@for(i in rock.cols.indices) {
+                    for (j in rock.cols[i].indices) {
+                        if (chamber[rx + i][ry - 1 + j].and(rock.cols[i][j])) {
+                            resting = true
+                            break@outer
+                        }
+                    }
+                }
+            }
+
+            if (resting) {
+                for (i in rock.cols.indices) {
+                    for (j in rock.cols[i].indices) {
+                        chamber[rx + i][ry + j] = chamber[rx + i][ry + j] or rock.cols[i][j]
+                    }
+                }
+            } else {
+                ry--
             }
         }
-        return false
+
+        var found = -1
+        for (i in rock.cols.first().indices) {
+            if (chamber.all { it[i + ry] }) found = i + ry
+        }
+        return max(y - 3, ry + rock.cols.first().size) to found
     }
 
     override fun problem2() {
+        val pattern = javaClass.getResource("/day17.txt")!!.readText().map {
+            if (it == '<') -1
+            else 1
+        }
+        val winds = Winds(pattern)
+        var chamber = createChamber()
+        var y = 3
 
+
+        val hashes = mutableListOf<Int>()
+        var saved = 0L
+
+        var patternFound = false
+        for (i in 0..2021) {
+            val index = i.mod(rocks.size)
+            val (ny, fy) = dropRock(rocks, index, y, chamber, winds, hashes)
+            if (fy != -1) {
+                saved += fy
+                y = fy + 4
+                chamber = createChamber()
+            } else {
+                y = ny + 3
+            }
+
+        }
+        println("max: ${(saved + y) - 3}")
+
+
+
+    }
+
+    private fun createChamber() = mutableListOf(
+        RockColumn(),
+        RockColumn(),
+        RockColumn(),
+        RockColumn(),
+        RockColumn(),
+        RockColumn(),
+        RockColumn(),
+    )
+}
+
+class RockColumn {
+    private val map = mutableMapOf<Int, Boolean>()
+
+    operator fun get(index: Int) = map[index] ?: false
+    operator fun set(index: Int, value: Boolean) {
+        map[index] = value
     }
 }
 
-data class Winds(val pattern: List<Int>, private var i: Int = 0) {
+data class Winds(val pattern: List<Int>, var i: Int = 0) {
     fun getNext(): Int {
         val n = pattern[i++]
         i = i.mod(pattern.size)
@@ -74,6 +164,8 @@ data class Winds(val pattern: List<Int>, private var i: Int = 0) {
     }
 }
 @Suppress("ArrayInDataClass")
-private data class Rock(val heights: IntArray, val hitbox: IntArray) {
-    fun width() = heights.size
+private data class Rock(val cols: List<BooleanArray>) {
+    fun width() = cols.size
 }
+
+private class RestingRock(val x: Int, val y: Int, rock: Rock)
